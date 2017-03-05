2d222c43309245191b1cc83b8a922c4d0206854b
Tyler <tylerlantern@gmail.com>
2017-03-05 15:09:42 +0700

    Initial Commit

---
+++ BaconTests/BaconTests.swift
@@ -0,0 +1,36 @@
+//
+//  BaconTests.swift
+//  BaconTests
+//
+//  Created by Nattapong Unaregul on 3/5/2560 BE.
+//  Copyright © 2560 Nattapong Unaregul. All rights reserved.
+//
+
+import XCTest
+@testable import Bacon
+
+class BaconTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
