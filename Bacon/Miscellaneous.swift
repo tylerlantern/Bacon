3e3b789e37744df8843197801817de19151a1d10
Tyler <tylerlantern@gmail.com>
2017-03-28 00:54:41 +0700

    initiate

---
+++ Bacon/Miscellaneous.swift
@@ -0,0 +1,9 @@
+//
+//  Miscellaneous.swift
+//  Bacon
+//
+//  Created by Nattapong Unaregul on 3/18/2560 BE.
+//  Copyright © 2560 Nattapong Unaregul. All rights reserved.
+//
+
+import Foundation
